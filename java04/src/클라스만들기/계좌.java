c9b7cc761dd1548461661c2b1996796b559477cf
hyelimhong <113962979+hyelimhong@users.noreply.github.com>
2022-10-04 14:01:54 +0900

    Add files via upload

---
+++ java04/src/클라스만들기/계좌.java
@@ -0,0 +1,18 @@
+package 클라스만들기;
+
+public class 계좌 {
+	
+	
+	
+		public String 이름;
+		public String 계좌이름;
+		public int 금액;
+		
+		
+		public void 예금하다() {
+			System.out.println("게좌 [이름=" + 이름 + ", 계좌이름=" + 계좌이름 + ", 금액=" + 금액 + "]");
+		}
+		public void 적금하다() {
+			System.out.println("게좌 [이름=" + 이름 + ", 계좌이름=" + 계좌이름 + ", 금액=" + 금액 + "]");
+		}
+}
